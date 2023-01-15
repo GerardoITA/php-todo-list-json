08f14bb8ff17e21f1e4412a5db2f1f918d70b559
GerardoITA <gerardo.giusti@tiscali.it>
2023-01-15 18:42:30 +0100

    sistemato il "template"

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,17 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import ToDo from './components/ToDo.vue'
+export default {
+  name: "App",
+  components: {
+    ToDo,
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}  
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ToDo></ToDo>
 </template>
 
 <style scoped>
